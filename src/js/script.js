0d5b7f43081ff32563b2d5781b111c3b431eed00
Akara184 <davisuporterj@gmail.com>
2023-05-21 22:47:22 -0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+//mobile menu
+
+const burgerIcon = document.querySelector(".navbar-burger");
+const navbarMenu = document.querySelector(".navbar-menu");
+
+burgerIcon.addEventListener('click', () => {
+    navbarMenu.classList.toggle("is-active");
+    burgerIcon.classList.toggle("is-active");
+
+});
