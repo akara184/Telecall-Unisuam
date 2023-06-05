82d62f96a0c2cdd825712ff4ce0b2f827bf5398d
Akara184 <davisuporterj@gmail.com>
2023-06-04 23:30:42 -0300

    Login completo, porém sem ancoragem

--- src/js/login.js
+++ src/js/login.js
@@ -10,3 +10,34 @@ burgerIcon.addEventListener('click', () => {
 });
 
 
+$("#entrarcor").click(function(){
+    var login = $("#login").val();
+    var senha = $("#senha").val();
+    
+    if (login == ""){
+        $("#login").focus();
+        bulmaToast.toast({ 
+            message: 'DIGITE UM LOGIN', 
+            type: 'is-link', 
+            position: 'bottom-center', 
+            pauseOnHover: true,
+            dismissible: true,
+            closeOnClick: true,
+            opacity: 1, })
+        return;
+    }
+    else if (senha == ""){
+        $("#senha").focus();
+        $("#login").focus();
+        bulmaToast.toast({ 
+            message: 'DIGITE UMA SENHA', 
+            type: 'is-link', 
+            position: 'bottom-center', 
+            pauseOnHover: true,
+            dismissible: true,
+            closeOnClick: true,
+            opacity: 1, })
+        return;
+
+    }
+})
