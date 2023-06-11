7124ec8c0685802b71f33aaa48c1e44b9a3f1bc5
Akara184 <davisuporterj@gmail.com>
2023-06-10 23:50:37 -0300

    Adicionando o slide

--- src/js/login.js
+++ src/js/login.js
@@ -1,37 +1,35 @@
-$("#entrarcor").click(function(){
-    var login = $("#login").val();
-    var senha = $("#senha").val();
-    
-    if (login == ""){
-        $("#login").focus();
-        bulmaToast.toast({ 
-            message: 'DIGITE UM LOGIN', 
-            type: 'is-link', 
-            position: 'bottom-center', 
-            pauseOnHover: true,
-            dismissible: true,
-            closeOnClick: true,
-            opacity: 1, })
-        return;
-    }
-    else if (senha == ""){
-        $("#senha").focus();
-        $("#login").focus();
-        bulmaToast.toast({ 
-            message: 'DIGITE UMA SENHA', 
-            type: 'is-link', 
-            position: 'bottom-center', 
-            pauseOnHover: true,
-            dismissible: true,
-            closeOnClick: true,
-            opacity: 1, })
-        return;
+$("#entrarcor").click(function () {
+  var login = $("#login").val();
+  var senha = $("#senha").val();
 
-    }
-//LOCAL STORAGE
-    localStorage.setItem("login", login);
-    localStorage.setItem("senha", senha);
-  
-    window.location = "/src/pages/main/index.html";
-})
+  if (login == "") {
+    $("#login").focus();
+    bulmaToast.toast({
+      message: "Digite um login",
+      type: "is-link",
+      position: "bottom-center",
+      pauseOnHover: true,
+      dismissible: true,
+      closeOnClick: true,
+      opacity: 1,
+    });
+    return;
+  } else if (senha == "") {
+    $("#senha").focus();
+    bulmaToast.toast({
+      message: "Digite uma senha",
+      type: "is-link",
+      position: "bottom-center",
+      pauseOnHover: true,
+      dismissible: true,
+      closeOnClick: true,
+      opacity: 1,
+    });
+    return;
+  }
+  //LOCAL STORAGE
+  localStorage.setItem("login", login);
+  localStorage.setItem("senha", senha);
 
+  window.location = "/src/pages/main/index.html";
+});
