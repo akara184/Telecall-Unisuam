1aa447101dadb128da3e4d288398882f5f04ac63
Akara184 <davisuporterj@gmail.com>
2023-06-17 11:42:05 -0300

    Arrumando o zoom dark mode

--- src/js/acessibilidade.js
+++ src/js/acessibilidade.js
@@ -1,4 +1,27 @@
 $(document).ready(function () {
+  $('#dark-mode-toggle').click(function () {
+    $('body, .navbar, .section, .card-content').toggleClass('is-dark');
+    $('.pricing-plan').toggleClass('is-black');
+    $('body, .section, .hero, .card, .pricing-table, .plan-header, .plan-item, .plan-items, .plan-footer, .plan-price').toggleClass('dark-mode');
+    $('.card-content, #contact, .copyright').toggleClass('has-background-dark');
+    $('title, span, p, h1, a, .title, .card-content').toggleClass('has-text-white');
+    var moonIcon = $('.fa-moon');
+    var islink = $(".is-link");
+    if (moonIcon.length) {
+      moonIcon.removeClass('fa-moon').addClass('fa-sun');
+      $('#logoid img').attr('src', '/src/images/telecall-branco.png');
+      islink.removeClass('is-link').addClass('is-black');
+
+    } else {
+      $('.fa-sun').removeClass('fa-sun').addClass('fa-moon');
+      $('#logoid img').attr('src', '/src/images/telecall-logo-removebg-preview.png');
+      $(".is-black").removeClass("is-black").addClass("is-link");
+    }
+  });
+});
+
+
+$(document).ready(function () {
   // Define o tamanho da fonte inicial
   var fontSize = 24;
 
@@ -21,28 +44,6 @@ $(document).ready(function () {
     if (fontSize > minFontSize) {
       fontSize -= fontSizeIncrement;
       $('p, h1, span, input, li a').css('font-size', fontSize + 'px');
-    }
-  });
-});
-
-$(document).ready(function () {
-  $('#dark-mode-toggle').click(function () {
-    $('body, .navbar, .section, .card-content').toggleClass('is-dark');
-    $('.pricing-plan').toggleClass('is-black');
-    $('body, .section, .hero, .card, .pricing-table, .plan-header, .plan-item, .plan-items, .plan-footer, .plan-price').toggleClass('dark-mode');
-    $('.card-content, #contact, .copyright').toggleClass('has-background-dark');
-    $('title, span, p, h1, a, .title, .card-content').toggleClass('has-text-white');
-    var moonIcon = $('.fa-moon');
-    var islink = $(".is-link");
-    if (moonIcon.length) {
-      moonIcon.removeClass('fa-moon').addClass('fa-sun');
-      $('#logoid img').attr('src', '/src/images/telecall-branco.png');
-      islink.removeClass('is-link').addClass('is-black');
-
-    } else {
-      $('.fa-sun').removeClass('fa-sun').addClass('fa-moon');
-      $('#logoid img').attr('src', '/src/images/telecall-logo-removebg-preview.png');
-      $(".is-black").removeClass("is-black").addClass("is-link");
     }
   });
 });
